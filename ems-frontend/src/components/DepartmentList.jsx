c5f62fdd4c33b56d074ff88cbbaa0e21d2c1445d
kumarmudur <kumarmudur10@gmail.com>
2024-01-17 13:00:36 -0500

    [Frontend] departmentList component changes

--- ems-frontend/src/components/DepartmentList.jsx
+++ ems-frontend/src/components/DepartmentList.jsx
@@ -1,34 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import { deleteDepartment, getAllDepartments } from '../services/departmentService';
 
 const DepartmentList = () => {
-    const testData = [
-        {
-            "id": 1,
-            "departmentName": "R&D",
-            "departmentDescription": "Research and Development Department"
-        },
-        {
-            "id": 2,
-            "departmentName": "Platform",
-            "departmentDescription": "Platform Department"
-        },
-        {
-            "id": 3,
-            "departmentName": "Finance",
-            "departmentDescription": "Finance Department"
-        }
-    ]
-    const [departments, setDepartments] = useState(testData);
+    const [departments, setDepartments] = useState([]);
+    const navigator = useNavigate();
+
+    useEffect(() => {
+        getAllDepartmentsData();
+    }, []);
+
+    const getAllDepartmentsData = () => {
+        getAllDepartments()
+        .then(response => setDepartments(response.data))
+        .catch(error => console.log(error));
+    }
+
+    const updateEmployee = id => {
+        navigator(`/edit-department/${id}`);
+    }
+
+    const removeEmployee = id => {
+        deleteDepartment(id).then(() => {
+            getAllDepartmentsData();
+        }).catch(error => console.log(error));
+     }
 
     return (
         <div className="container">
             <h2 className="text-center">List of Departments</h2>
+            <Link to="/add-department" className="btn btn-primary mb-2">Add Department</Link>
             <table className="table table-striped table-bordered">
                 <thead>
                     <tr>
                         <th>Department Id</th>
                         <th>Department Name</th>
                         <th>Department Description</th>
+                        <th>Actions</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -38,6 +47,20 @@ const DepartmentList = () => {
                                 <td>{id}</td>
                                 <td>{departmentName}</td>
                                 <td>{departmentDescription}</td>
+                                <td>
+                                    <button 
+                                        className="btn btn-info"
+                                        onClick={() => updateEmployee(id)}
+                                        >Update
+                                    </button>
+                                    <button 
+                                       className="btn btn-danger"
+                                       onClick={() => removeEmployee(id)}
+                                       style={{marginLeft: '10px'}}
+                                    >
+                                        Delete
+                                    </button>
+                                </td>
                             </tr>
                         ))
                     }
