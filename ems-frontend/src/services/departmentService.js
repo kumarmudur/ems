e809d56ea8309f24438cf4bbc43f9902033db27f
kumarmudur <kumarmudur10@gmail.com>
2024-01-19 16:56:47 -0500

    [Frontend] department service changes

--- ems-frontend/src/services/departmentService.js
+++ ems-frontend/src/services/departmentService.js
@@ -1,5 +1,13 @@
 import axios from "axios";
 
-const BASE_URL = 'http://localhost:8080/api/departments';
+import { DEPARTMENT_URL } from '../constants/api';
 
-export const getAllDepartments = () => axios.get(BASE_URL);
+export const getAllDepartments = () => axios.get(DEPARTMENT_URL);
+
+export const getDepartment = id => axios.get(`${DEPARTMENT_URL}/${id}`);
+
+export const createDepartment = (payload) => axios.post(DEPARTMENT_URL, payload);
+
+export const updateDepartment = (id, payload) => axios.put(`${DEPARTMENT_URL}/${id}`, payload);
+
+export const deleteDepartment = id => axios.delete(`${DEPARTMENT_URL}/${id}`);
