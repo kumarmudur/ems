90b3480573b755660307d362c68608e910c77c81
kumarmudur <kumarmudur10@gmail.com>
2024-01-20 22:24:17 -0500

    [Frontend] footer component changes

---
+++ ems-frontend/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <span>All rights reserved 2023 by kumarmudur.com</span>
+        </footer>
+    );
+};
+
+export default Footer;
